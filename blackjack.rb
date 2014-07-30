cf86c151e210275c0ed11d3905febd471042925e
JZ <johnomatopoeia@users.noreply.github.com>
2014-07-30 13:00:25 -0400

    Update blackjack.rb

--- blackjack.rb
+++ blackjack.rb
@@ -1,12 +1,15 @@
-
+#Variables:
 player_name = "BossMan"
-player_hand = []
-dealer_hand = []
+player_total = 0
+player_cards = []
+dealer_cards = []
+dealer_total = 0
 new_player = 1
 bankroll = 0
+bet = 0
 deck = []
 
-def bankroll(bet,new_player)
+def bankroll_calc(bankroll, bet,new_player)
   if new_player == 1
     bankroll = 2000
   else
@@ -15,29 +18,52 @@ def bankroll(bet,new_player)
   return bankroll
 end
 
+def place_bet(bankroll, bet)
+  bankroll -= bet
+  return bankroll
+end
+
 
 def shuffle_decks(deck)
   suits = ["spades","clubs","diamonds", "hearts"]
   cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
+  #combine suits and cards 4 times to create 4 decks
   suits.each do |suit|
-    cards.each {|c| deck << "#{c.to_s}-#{suit}"}
+  4.times do cards.each {|c| deck << "#{c.to_s}-#{suit}"} end
     end
-    return deck
-end
-
-def deal_cards()
-
+  4.times do deck.shuffle! end #might as well make sure they're shuffled :)
+  return deck
 end
 
 
+def deal_cards(player_cards, dealer_cards, deck)
+  player_cards << deck.shift
+  dealer_cards << deck.shift
+end
 
+def show_table(bankroll, bet, dealer_cards,player_cards, player_name)
+  puts "Bankroll = #{bankroll}  Bet = #{bet}"
+  puts "\n\n\n Dealer: \n #{dealer_cards}"
+  puts "\n\n\n #{player_name}: \n #{player_cards}"
+  
+end
 
-bankroll = bankroll(0,new_player)
-deck = shuffle_decks(deck)
-p deck
-p bankroll
+bankroll = bankroll_calc(bankroll, bet,new_player)
+shuffle_decks(deck)
 
+puts "Place your bet."
+bet = 100 #gets.chomp.to_i
+
+bankroll = place_bet(bankroll, bet)
+
+2.times do deal_cards(player_cards, dealer_cards, deck) end
+
+puts player_cards
+puts dealer_cards
+puts deck.count
+
+show_table(bankroll, bet, dealer_cards,player_cards, player_name)
 
 
 
